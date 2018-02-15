03d5b090e82dbcc7ac847d8a470eb047d58c309f
Julien Derivière <github@gradew.net>
2018-02-15 11:06:59 +0100

    Delete transmit.cpp

--- transmit.cpp
+++
@@ -1,9 +0,0 @@
-#include "../gradewrf.h"
-
-int main(int argc, char *argv[])
-{
-        GradewRF rf;
-        rf.setTransmit(17); // Use Broadcom chip 17
-        rf.transmitData(0x01020304);
-        return 0;
-}
